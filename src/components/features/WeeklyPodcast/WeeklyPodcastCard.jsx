e5903235d5d475fd12ddfc08fe615eb949c12cf3
Grace <Grace@fakeemail.com>
2026-03-10 01:48:55 +0800

    feat: 完成 WeeklyPodcastCard 元件及樣式

---
+++ src/components/features/WeeklyPodcast/WeeklyPodcastCard.jsx
@@ -0,0 +1,30 @@
+import "./WeeklyPodcastCard.scss";
+import PlayButton from "@/components/common/button/PlayButton";
+
+const WeeklyPodcastCard = ({ weeklyPodcasts }) => {
+  const data = weeklyPodcasts[0];
+  const { id, image, title, author, duration } = data;
+
+  return (
+    <div className="weekly-podcast-card">
+      {/* 左側圖片 */}
+      <div className="weekly-podcast-card__img">
+        <img src={image} alt="weekly-podcast-card-image" />
+      </div>
+      {/* 右側資訊 */}
+      <div className="weekly-podcast-card__content">
+        <h5 className="weekly-podcast-card__content__title h5">{title}</h5>
+        <p className="weekly-podcast-card__content__authod body4">{author}</p>
+        <p className="weekly-podcast-card__content__duration body1">
+          {duration}
+        </p>
+      </div>
+      {/* 播放鍵 */}
+      <div className="weekly-podcast-card__btn">
+        <PlayButton variant="secondary"></PlayButton>
+      </div>
+    </div>
+  );
+};
+
+export default WeeklyPodcastCard;
