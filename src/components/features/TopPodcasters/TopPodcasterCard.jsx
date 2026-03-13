ca355d4490a835db16c4bed564952d3d2b92ab94
Grace <Grace@fakeemail.com>
2026-03-14 00:32:46 +0800

    TopPodcasterCard 基礎樣式完成

---
+++ src/components/features/TopPodcasters/TopPodcasterCard.jsx
@@ -0,0 +1,39 @@
+import "./TopPodcasterCard.scss";
+
+const TopPodcasterCard = ({ size = "" }) => {
+  const data = {
+    id: "top-2",
+    image: `${import.meta.env.BASE_URL}images/topPodcast/top-podcaster-2.png`,
+    author: "Nadia Lestary",
+    streams: null,
+  };
+
+  const formatStreams = (streams) => {
+    if (!streams) return null;
+    if (streams >= 1000000)
+      return parseFloat((streams / 1000000).toFixed(1)) + "million";
+    if (streams >= 1000)
+      return parseFloat((streams / 1000).toFixed(1)) + "Kilo";
+    return streams.toString();
+  };
+
+  return (
+    <div className={`top-podcaster-card top-podcaster-card--${size}`}>
+      <div className="top-podcaster-card__info">
+        <h2
+          className={`top-podcaster-card__author top-podcaster-card__author--${size} h4`}
+        >
+          {data.author}
+        </h2>
+        {size === "large" && data.streams && (
+          <p className="top-podcaster-card__streams">
+            {formatStreams(data.streams)} streams
+          </p>
+        )}
+      </div>
+      <img src={data.image} alt="top-podcaster-author" />
+    </div>
+  );
+};
+
+export default TopPodcasterCard;
