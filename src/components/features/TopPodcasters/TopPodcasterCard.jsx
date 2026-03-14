66d08b53dda79d39c3217891eed880badc939ed0
Grace <Grace@fakeemail.com>
2026-03-15 00:36:10 +0800

    style(TopPodcasters): 完成TopPodcasters RWD 樣式

--- src/components/features/TopPodcasters/TopPodcasterCard.jsx
+++ src/components/features/TopPodcasters/TopPodcasterCard.jsx
@@ -6,17 +6,19 @@ const TopPodcasterCard = ({ data, size }) => {
   const formatStreams = (streams) => {
     if (!streams) return null;
     if (streams >= 1000000)
-      return parseFloat((streams / 1000000).toFixed(1)) + "million";
+      return parseFloat((streams / 1000000).toFixed(1)) + " million";
     if (streams >= 1000)
-      return parseFloat((streams / 1000).toFixed(1)) + "Kilo";
+      return parseFloat((streams / 1000).toFixed(1)) + " Kilo";
     return streams.toString();
   };
 
   return (
     <div className={`top-podcaster-card top-podcaster-card--${size}`}>
-      <div className="top-podcaster-card__info">
+      <div
+        className={`top-podcaster-card__info top-podcaster-card__info--${size}`}
+      >
         <h2
-          className={`top-podcaster-card__author top-podcaster-card__author--${size} h4`}
+          className={`top-podcaster-card__author top-podcaster-card__author--${size}`}
         >
           {author}
         </h2>
